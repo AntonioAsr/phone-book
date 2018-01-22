f8815140d87b3c3890dc183bf7478359524ccdd1
sego <antoniosegoviaruiz@gmail.com>
2018-01-22 18:47:54 +0000

    Added Material-UI library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: true};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+  
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div>
+            <RaisedButton
+              label="Toggle Drawer"
+              onClick={this.handleToggle}
+            />
+            <Drawer open={this.state.open}>
+              <MenuItem>Menu Item</MenuItem>
+              <MenuItem>Menu Item 2</MenuItem>
+            </Drawer>
+        </div>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
